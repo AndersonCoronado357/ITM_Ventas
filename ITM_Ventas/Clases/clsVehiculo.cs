a491939e37a5a0a5576650c0186d464014e2851a
Anderson Coronado <andersoncoronado157@gmail.com>
2025-03-12 01:28:12 -0500

    commit agencias y vehiculos

---
+++ ITM_Ventas/Clases/clsVehiculo.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ITM_Ventas.Models;
+using System.Data.Entity.Migrations;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ITM_Ventas.Clases
+{
+    public class clsVehiculo
+    {
+
+        private ITM_ventasEntities ITM_Ventas = new ITM_ventasEntities();
+
+        public Vehiculo vehiculo { get; set; }
+
+        public String Insertar()
+
+        {
+
+            try
+
+            {
+
+                ITM_Ventas.Vehiculoes.Add(vehiculo);
+
+                ITM_Ventas.SaveChanges();
+
+                return "vehiculo ingresado correctamente " + vehiculo.IdVehiculo;
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                return "error al insertar el vehiculo " + ex.Message;
+
+            }
+
+        }
+
+        public List<Vehiculo> ConsultarTodos()
+
+        {
+
+            return ITM_Ventas.Vehiculoes
+
+                .OrderBy(e => e.IdVehiculo)
+
+                .ToList();
+
+        }
+
+        public Vehiculo Consultar(int IdVehiculo)
+
+        {
+
+            Vehiculo veh = ITM_Ventas.Vehiculoes.FirstOrDefault(e => e.IdVehiculo == IdVehiculo);
+
+            return veh;
+
+        }
+
+        public String Actualizar()
+
+        {
+
+            Vehiculo veh = Consultar(vehiculo.IdVehiculo);
+
+            if (veh == null)
+
+            {
+
+                return "el vehiculo no existe";
+
+            }
+
+            ITM_Ventas.Vehiculoes.AddOrUpdate(vehiculo);
+
+            ITM_Ventas.SaveChanges();
+
+            return "se ha actualizado el vehiculo correctamente";
+        }
+
+
+
+        public String Eliminar()
+
+        {
+
+            try
+
+            {
+
+                Vehiculo veh = Consultar(vehiculo.IdVehiculo);
+
+                if (veh == null)
+
+                {
+
+                    return "el vehiculo no existe";
+
+                }
+
+                ITM_Ventas.Vehiculoes.Remove(veh);
+
+                ITM_Ventas.SaveChanges();
+
+                return "se ha eliminado el vehiculo correctamente";
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                return ex.Message;
+
+            }
+
+        }
+
+        public String EliminarXId(int IdVehiculo)
+
+        {
+
+            try
+
+            {
+
+                Vehiculo veh = Consultar(IdVehiculo);
+
+                if (veh == null)
+
+                {
+
+                    return "el vehiculo no existe";
+
+                }
+
+                ITM_Ventas.Vehiculoes.Remove(veh);
+
+                ITM_Ventas.SaveChanges();
+
+                return "se ha eliminado el vehiculo correctamente";
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                return ex.Message;
+
+            }
+
+
+        }
+
+    }
+
+}
---
+++ ITM_Ventas/Controllers/VehiculosController.cs
@@ -0,0 +1,67 @@
+﻿using ITM_Ventas.Clases;
+using ITM_Ventas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ITM_Ventas.Controllers
+{
+    [RoutePrefix("api/Vehiculos")]
+    public class VehiculosController : ApiController
+    {
+
+        [HttpGet]
+        [Route("ConsultarTodos")]
+        public List<Vehiculo> ConsultarTodos()
+        {
+            clsVehiculo Vehiculo = new clsVehiculo();
+            return Vehiculo.ConsultarTodos();
+        }
+
+        [HttpGet]
+        [Route("ConsultarXId")]
+        public Vehiculo ConsultarXId(int IdVehiculo)
+        {
+            clsVehiculo Vehiculo = new clsVehiculo();
+            return Vehiculo.Consultar(IdVehiculo);
+        }
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] Vehiculo vehiculo)
+        {
+            clsVehiculo Vehiculo = new clsVehiculo();
+            Vehiculo.vehiculo = vehiculo;
+            return Vehiculo.Insertar();
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Vehiculo vehiculo)
+        {
+            clsVehiculo Vehiculo = new clsVehiculo();
+            Vehiculo.vehiculo = vehiculo;
+            return Vehiculo.Actualizar();
+        }
+
+        [HttpDelete]
+        [Route("Eliminar")]
+        public string Eliminar([FromBody] Vehiculo vehiculo)
+        {
+            clsVehiculo Vehiculo = new clsVehiculo();
+            Vehiculo.vehiculo = vehiculo;
+            return Vehiculo.Eliminar();
+        }
+
+        [HttpDelete]
+        [Route("EliminarXId")]
+        public string EliminarXId(int IdVehiculo)
+        {
+            clsVehiculo Vehiculo = new clsVehiculo();
+            return Vehiculo.EliminarXId(IdVehiculo);
+        }
+    }
+}
